9876897b3a16f9ce414fe9e346c85cc7d0e8b7f3
peet <teilzeitjobbewerber@gmx.de>
2023-12-27 09:43:39 +0100

    onclick color changes added

--- js/templates.js
+++ js/templates.js
@@ -1,22 +1,21 @@
 function sideBarTabsHTML(){
   return `
   <div id="sidebar-tab" class="tab0" onclick="changeClickedTab(0)">
-  <img src="/assets/img//header_sidebar/icon_summary_grey.png" id="tab-img0" alt="">
-  <span id="sidebar-text">Summary</span>
-</div>
-<div id="sidebar-tab" class="tab1" onclick="changeClickedTab(1)">
-  <img src="/assets/img//header_sidebar/icon_add_task_grey.png" alt="">
-  <span id="sidebar-text">Add Task</span>
-</div>
-<a href="./board.html">
-<div id="sidebar-tab" class="tab2" onclick="changeClickedTab(2)">
-  <img src="/assets/img//header_sidebar/icon_board_grey.png" alt="">
-  <span id="sidebar-text">Board</span>
-</div>
-</a>
-<div id="sidebar-tab" class="tab3" onclick="changeClickedTab(3)">
-  <img src="/assets/img//header_sidebar/icon_contacts_grey.png" alt="">
-  <span id="sidebar-text">Contacts</span>
-</div>
+    <img src="/assets/img//header_sidebar/icon_summary_grey.png" id="tab-img0" alt="">
+    <span id="sidebar-text">Summary</span>
+  </div>
+  <div id="sidebar-tab" class="tab1" onclick="changeClickedTab(1)">
+    <img src="/assets/img//header_sidebar/icon_add_task_grey.png" id="tab-img1" alt="">
+    <span id="sidebar-text">Add Task</span>
+  </div>
+  <div id="sidebar-tab" class="tab2" onclick="changeClickedTab(2)">
+    <img src="/assets/img//header_sidebar/icon_board_grey.png" id="tab-img2" alt="">
+    <span id="sidebar-text">Board</span>
+  </div>
+  <div id="sidebar-tab" class="tab3" onclick="changeClickedTab(3)">
+    <img src="/assets/img//header_sidebar/icon_contacts_grey.png" id="tab-img3" alt="">
+    <span id="sidebar-text">Contacts</span>
+  </div>
+
   `
 }
--- script.js
+++ script.js
@@ -13,9 +13,21 @@ async function includeHTML() {
   }
 }
 
+let whiteImageCorrespondends = {
+  0: "./assets/img/header_sidebar/icon_summary_white.png",
+  1: "./assets/img/header_sidebar/icon_add_task_white.png",
+  2: "./assets/img/header_sidebar/icon_board_white.png",
+  3: "./assets/img/header_sidebar/icon_contacts_white.png"
+}
+
 function changeClickedTab(i) {
-  let sideBarTabs = document.getElementById(`sidebar-tabs`)
-  sideBarTabs.innerHTML = ""
-  sideBarTabs.innerHTML = "" 
-  let clickedTab = document.querySelector(`.tab${i}`)
+  console.log(i);
+  let sideBarTabs = document.getElementById(`sidebar-tabs`);
+  sideBarTabs.innerHTML = "";
+  sideBarTabs.innerHTML = sideBarTabsHTML();
+  let clickedTab = document.querySelector(`.tab${i}`);
+  clickedTab.style.color = "white";
+  clickedTab.style.backgroundColor =  "#091931"
+  let clickedTabImg = document.getElementById(`tab-img${i}`);
+  clickedTabImg.src = whiteImageCorrespondends[i];
 }
