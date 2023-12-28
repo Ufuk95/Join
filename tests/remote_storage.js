b6e2f29b83ad569ab14d24ee2c60fb5ee1eee020
peet <teilzeitjobbewerber@gmx.de>
2023-12-28 18:03:45 +0100

    remote storage added

--- tests/remote_storage.js
+++ tests/remote_storage.js
@@ -1,3 +1,30 @@
 
 // Tests for remote storage
 
+
+// generate tokens at  https://remote-storage.developerakademie.org/token-generator
+const STORAGE_TOKEN = '8RLSPP294RL2SQXQDY9KO3BLXKSMB4WUNVXGDP2S';
+const STORAGE_URL = 'https://remote-storage.developerakademie.org/item';
+
+
+
+/**
+ * Sends key value pair to a backend
+ * Simultaniously returns that data as an object/json
+ */
+async function setItem(key, value) {
+    const payload = { key, value, token: STORAGE_TOKEN };
+    return fetch(STORAGE_URL, { method: 'POST', body: JSON.stringify(payload)})
+    .then(res => res.json());
+}
+
+async function getItem(key) {
+    const url = `${STORAGE_URL}?key=${key}&token=${STORAGE_TOKEN}`;
+    return fetch(url).then(res => res.json());
+}
+
+async function myData(){
+  console.log(await getItem("animal"));
+}
+
+myData()
