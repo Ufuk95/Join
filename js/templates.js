0d88604fef59ac67fd713e47fa73a056efa2980a
peet <teilzeitjobbewerber@gmx.de>
2024-01-06 10:57:02 +0100

    functioning version

--- js/templates.js
+++ js/templates.js
@@ -12,13 +12,17 @@ function sideBarTabsHTML(){
       <span id="sidebar-text">Add Task</span>
     </div>
   </a>
-  <div id="sidebar-tab" class="tab2" onclick="changeClickedTab(2)">
-    <img src="./assets/img//header_sidebar/icon_board_grey.png" id="tab-img2" alt="">
-    <span id="sidebar-text">Board</span>
-  </div>
-  <div id="sidebar-tab" class="tab3" onclick="changeClickedTab(3)">
-    <img src="./assets/img//header_sidebar/icon_contacts_grey.png" id="tab-img3" alt="">
-    <span id="sidebar-text">Contacts</span>
-  </div>
+  <a href="./board.html">
+    <div id="sidebar-tab" class="tab2" onclick="changeClickedTab(2)">
+      <img src="./assets/img//header_sidebar/icon_board_grey.png" id="tab-img2" alt="">
+      <span id="sidebar-text">Board</span>
+    </div>
+  </a>
+  <a href="./contacts.html">
+    <div id="sidebar-tab" class="tab3" onclick="changeClickedTab(3)">
+      <img src="./assets/img//header_sidebar/icon_contacts_grey.png" id="tab-img3" alt="">
+      <span id="sidebar-text">Contacts</span>
+    </div>
+  </a>
   `
 }
--- script.js
+++ script.js
@@ -11,7 +11,6 @@ async function includeHTML() {
   for (let i = 0; i < includeElements.length; i++) {
     const element = includeElements[i];
     file = element.getAttribute("w3-include-html");
-    // "includes/header.html"
     let resp = await fetch(file);
     if (resp.ok) {
       element.innerHTML = await resp.text();
