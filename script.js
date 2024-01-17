fefc7a80b2e1e1cdba2738c6272fea49d8e66b3d
Ufuk Özsahin <oezsahin95@icloud.com>
2024-01-17 14:48:52 +0100

    async

--- script.js
+++ script.js
@@ -6,16 +6,22 @@ async function loadAll() {
 }
 
 async function includeHTML() {
-  let includeElements = document.querySelectorAll('[w3-include-html]');
-  for (let i = 0; i < includeElements.length; i++) {
-    const element = includeElements[i];
-    file = element.getAttribute("w3-include-html");
-    let resp = await fetch(file);
-    if (resp.ok) {
-      element.innerHTML = await resp.text();
-    } else {
-      element.innerHTML = 'Page not found';
-    }
+  const headerResponse = await fetch('./assets/templates/header.html');
+  const headerHTML = await headerResponse.text();
+  const headerElement = document.getElementById('header');
+  if (headerElement) {
+      headerElement.innerHTML = headerHTML;
+  } else {
+      console.error('Header element not found.');
+  }
+
+  const sidebarResponse = await fetch('./assets/templates/sidebar.html');
+  const sidebarHTML = await sidebarResponse.text();
+  const sidebarElement = document.getElementById('sidebar');
+  if (sidebarElement) {
+      sidebarElement.innerHTML = sidebarHTML;
+  } else {
+      console.error('Sidebar element not found.');
   }
 }
 
