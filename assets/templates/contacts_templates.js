8abecf9519d327007d3ba2d1812db37ba92229e5
peet <teilzeitjobbewerber@gmx.de>
2024-01-21 12:16:20 +0100

    functioning version

---
+++ assets/templates/contacts_templates.js
@@ -0,0 +1,17 @@
+function contactFrameHTML(){
+  return`
+  <div class="contact-frame">
+  <div class="name-circle-wrapper">
+    <svg width="42" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <circle cx="21" cy="21" r="20" fill="#FF7A00" stroke="white" stroke-width="2" />
+    </svg>
+    <span class="initials">AB</span>
+  </div>
+  <div class="name-email-box">
+    <span class="name-email-box__name">Peter Ediger</span>
+    <span class="name-email-box__mail">peter@gmx.de</span>
+  </div>
+</div>
+
+  `
+}
