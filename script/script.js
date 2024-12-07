e7575507ecb44e324c54728fece1d77daa5e0adf
Ufuk Özsahin <oezsahin95@icloud.com>
2024-12-07 19:41:51 +0100

    backend auth. fix

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 
 const USERS_URL = "https://ufuks-new-join-user-default-rtdb.europe-west1.firebasedatabase.app/";
-const TASKS_URL = "https://ufuk-new-join-task-default-rtdb.europe-west1.firebasedatabase.app/";
+const TASKS_URL = "https://ufuk-new-join-task-fbe7b-default-rtdb.europe-west1.firebasedatabase.app/";
 const CONTACTS_URL = "https://ufuk-new-join-contact-7c920-default-rtdb.europe-west1.firebasedatabase.app/";
 
 
